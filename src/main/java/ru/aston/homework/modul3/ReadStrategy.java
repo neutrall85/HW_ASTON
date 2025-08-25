196645aa4867ca9f79e1fcb3934d24e86af534d1
neutrall85 <canni85@mail.ru>
2025-08-25 11:01:12 +0400

    add ReadStrategy.java, fix UserChoiceStrategy.java & WriteStrategy.java

--- src/main/java/ru/aston/homework/modul3/ReadStrategy.java
+++ src/main/java/ru/aston/homework/modul3/ReadStrategy.java
@@ -2,17 +2,15 @@ package ru.aston.homework.modul3;
 
 import java.util.Scanner;
 
-public class WriteStrategy implements UserChoiceStrategy {
+public class ReadStrategy implements UserChoiceStrategy {
     @Override
     public void invoke(Scanner scanner) {
         System.out.print("Введите путь к файлу: ");
         String filePath = scanner.nextLine();
-        System.out.print("Введите текст для записи: ");
-        String text = scanner.nextLine();
 
         try {
-            FileWriter writer = new FileWriter(filePath, text);
-            writer.process();
+            FileReader reader = new FileReader(filePath);
+            reader.process();
         } catch (MyFileException e) {
             System.err.println("Ошибка: " + e.getMessage());
         }
--- src/main/java/ru/aston/homework/modul3/UserChoiceStrategy.java
+++ src/main/java/ru/aston/homework/modul3/UserChoiceStrategy.java
@@ -1,5 +1,7 @@
 package ru.aston.homework.modul3;
 
+import java.util.Scanner;
+
 public interface UserChoiceStrategy {
-    void invoke();
+    void invoke(Scanner scanner) throws MyFileException;;
 }
