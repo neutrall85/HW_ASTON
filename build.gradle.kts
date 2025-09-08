19f2d12bda9aa6b92ce407bfeca520e17bff0808
neutrall85 <canni85@mail.ru>
2025-09-08 20:58:01 +0400

    fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+val junitVersion = "5.11.0"
+val junitApiVersion = "5.11.0"
+val mockitoVersion = "5.19.0"
+val lombokVersion = "1.18.38"
+val jetbrainsAnnotationsVersion = "23.0.0"
+val slf4jVersion = "2.0.7"
+val logbackVersion = "1.5.13"
+
 plugins {
     id("java")
     id("checkstyle")
@@ -11,14 +19,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitApiVersion")
+    testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    compileOnly ("org.projectlombok:lombok:1.18.38")
-    annotationProcessor ("org.projectlombok:lombok:1.18.38")
-    implementation("org.jetbrains:annotations:23.0.0")
-    implementation("org.slf4j:slf4j-api:2.0.7")
-    implementation("ch.qos.logback:logback-classic:1.5.13")
+    compileOnly ("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    implementation("org.jetbrains:annotations:$jetbrainsAnnotationsVersion")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 }
 
 tasks.test {
